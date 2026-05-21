580c602b5631097510759e8b590fb494f2c48d07
rathoresneha <rathoresneha2801@gmail.com>
2026-05-21 17:33:12 +0530

    updates

--- revenue/frontend/src/firebase/config.js
+++ revenue/frontend/src/firebase/config.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhECXZZi67f3D_zciN86Xxk6AUdKwH4_k",
-  authDomain: "matrabhumiweb.firebaseapp.com",
-  databaseURL: "https://matrabhumiweb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "matrabhumiweb",
-  storageBucket: "matrabhumiweb.firebasestorage.app",
-  messagingSenderId: "733223433679",
-  appId: "1:733223433679:web:cb6fd31bb61094a83b8eba",
-  measurementId: "G-R943KVB43Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 let app;
